01046c65e11288697c6969eb5be365e7bef83caf
tmbc12 <reachus@tmbc.in>
2024-02-05 14:43:11 +0530

    first commit

---
+++ src/const/meta.ts
@@ -0,0 +1,9 @@
+import { MetaData } from '@/types/meta';
+
+export const DEFAULT_AVATAR = '🤖';
+export const DEFAULT_USER_AVATAR = '😀';
+export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
+export const DEFAULT_AGENT_META: MetaData = {};
+// export const DEFAULT_INBOX_AVATAR = '🤯';
+export const DEFAULT_USER_AVATAR_URL =
+  'https://mcusercontent.com/4d495ae0bbe9396d5b9e37d4a/images/10791233-7847-23ad-00f9-cc74e34e5434.png';
